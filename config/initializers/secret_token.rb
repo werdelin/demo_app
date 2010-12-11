0299e1fc12e667a22de537a8b4673bbf91d2a036
Your Name <henrik@prehype.com>
2010-12-11 15:42:11 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '4b48be77ed28d895149679e1c34ccdbdf5b8cc3781241182ff3607687a8b7011fe811c05143ef6d389c504e69650bfb3bbe3da5f269fb67f9df9618199ff6aad'
